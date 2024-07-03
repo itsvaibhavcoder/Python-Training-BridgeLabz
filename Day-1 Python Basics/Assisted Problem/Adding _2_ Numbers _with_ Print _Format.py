c44582f25c05c71218c4304cfb54f29478ead1b5
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-07-03 18:34:09 +0530

    Create Adding _2_ Numbers _with_ Print _Format.py

---
+++ Day-1 Python Basics/Assisted Problem/Adding _2_ Numbers _with_ Print _Format.py
@@ -0,0 +1,10 @@
+# PROG 1.2 Adding 2 Numbers with Print Format
+# Write the Assisted Practice Code Here
+import threading
+print(f"Main or Default Threads is {str(threading.current_thread().name)}")
+
+x = 100
+y = 100
+
+z = x + y
+print(f"The result of adding {x} and {y} is {z}")
