21ec36128035d9374fe7b49c60e8a59baadd029e
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-07-03 18:35:52 +0530

    Create Adding _2_ Numbers _Measuring _Time.py

---
+++ Day-1 Python Basics/Assisted Problem/Adding _2_ Numbers _Measuring _Time.py
@@ -0,0 +1,12 @@
+# PROG 1.3 Adding 2 Numbers Measuring Time
+# Write the Assisted Practice Code Here
+# Please ensure output as shown below
+
+import time
+start_time = time.time()
+a = 100
+b = 200
+c = a + b
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"The result of adding two number is {c} and time required to add them is {elapsed_time}")
