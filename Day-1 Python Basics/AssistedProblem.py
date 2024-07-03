6fac7a019414809d2be9f94cdf79c5cda02e327a
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-07-03 18:30:45 +0530

    Update AssistedProblem.py

--- Day-1 Python Basics/AssistedProblem.py
+++ Day-1 Python Basics/AssistedProblem.py
@@ -1,5 +1,11 @@
 # PROG 1: Hello World
-
 # Write the Assisted Practice Code Here
 print("Hello World")
+
+# PROG 2: Adding 2 Numbers Practice
+# Write the Assisted Practice Code Here
+a=5
+b=6
+c = a+b
+print(c)
 
