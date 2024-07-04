de02b35ca5f2d4cca92f1291cbaae45acc7febf1
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-07-04 12:54:53 +0530

    Create Descriptive_ Variable_ Name.py

---
+++ Day-1 Python Basics/Assisted Problem/Descriptive_ Variable_ Name.py
@@ -0,0 +1,10 @@
+# PROG 2: Descriptive Variable Name
+
+# Write the Assisted Practice Code Here
+# Please ensure output as shown below
+score_1 = 15
+score_2 = 15
+
+finale_score = score_1 + score_2
+
+print(f"The Finale score is {finale_score}")
